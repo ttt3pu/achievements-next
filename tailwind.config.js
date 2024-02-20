e151050bbc4f3579ea5d2e54f18a2ab79ba7301b
attt <ttt3pu@gmail.com>
2024-02-20 23:50:19 +0900

    refactor: Box.tsx with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: {
+          100: '#1c2135',
+          200: '#202940',
+          300: '#3f3c56',
+          DEFAULT: '#675878',
+          500: '#927699',
+        },
+      },
+    },
   },
   plugins: [],
 };
