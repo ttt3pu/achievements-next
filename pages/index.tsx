32f1470e08393f5bb8d77c3b04db1a56752381f2
attt <ttt3pu@gmail.com>
2024-02-22 01:04:06 +0900

    refactor: index.tsx with tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,19 +61,25 @@ export default function Home({ posts }: Props) {
   }
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.sortContainer}>
+    <div className="px-5 pb-5 overflow-hidden">
+      <div className="max-w-contents mx-auto mt-0 mb-5 p-2 flex flex-wrap bg-bg-200 rounded">
         {sortMenuItems.map((item, i) => {
           const isSelected = sortingKey === item.key;
           return (
-            <div
-              key={i}
-              className={styles.sortItem}
-              data-direction={sortingDirection === 'desc' ? 'desc' : 'asc'}
-              data-is-selected={isSelected}
-            >
-              {isSelected && <GoTriangleDown />}
-              <button onClick={() => onClickedSortButton(item.key)} className={styles.sortButton}>
+            <div key={i} className="relative">
+              {isSelected && (
+                <GoTriangleDown
+                  className={`pointer-events-none absolute top-[1px] bottom-0 left-[16px] m-auto z-10 transition-all fill-yellow ${
+                    sortingDirection === 'desc' ? '-rotate-180 ' : ''
+                  }`}
+                />
+              )}
+              <button
+                onClick={() => onClickedSortButton(item.key)}
+                className={`rounded cursor-pointer relative overflow-hidden bg-transparent text-bg border-0 px-5 py-1 transition-all${
+                  isSelected ? 'shadow bg-bg font-medium text-white pl-10' : ''
+                }`}
+              >
                 {item.text}
               </button>
             </div>
@@ -81,13 +87,16 @@ export default function Home({ posts }: Props) {
         })}
       </div>
 
-      <div className={styles.itemContainer}>
+      <div className="max-w-contents mx-auto mt-5 mb-0 pt-5 border-t border-t-bg grid grid-cols-6 gap-5 max-md:grid-cols-2">
         {posts.map((post, i) => {
           return (
             <Link key={i} href={`/${post.id}`} legacyBehavior>
-              <a className={styles.item} ref={postRefs.current[i]}>
-                <SteamBanner className={styles.itemBanner} steamId={post.steamId} />
-                <p>{i + 1}</p>
+              <a
+                className={`${styles.gridItem} mx-auto shadow bg-bg-200 rounded flex flex-col cursor-pointer text-white text-center font-medium overflow-hidden max-md:col-span-2 hover:z-10`}
+                ref={postRefs.current[i]}
+              >
+                <SteamBanner steamId={post.steamId} className="w-full h-full object-cover" />
+                <p className="p-1 bg-bg-300">{i + 1}</p>
               </a>
             </Link>
           );
