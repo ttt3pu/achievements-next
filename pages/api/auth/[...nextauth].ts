991cbfd9cf5eb00134e75a11adb4e60651927463
attt <ttt3pu@gmail.com>
2025-01-13 00:13:23 +0900

    feat: change OAuth provider to google

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,10 @@
 import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 export const authOptions = {
   providers: [
-    GithubProvider({
-      clientId: process.env.AUTH_CLIENT_ID,
-      clientSecret: process.env.AUTH_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: process.env.AUTH_CLIENT_ID_GOOGLE,
+      clientSecret: process.env.AUTH_CLIENT_SECRET_GOOGLE,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
